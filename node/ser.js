6bef3cc47eeaf87a92fe307e218b493f6e188294
Alejandro Romero <alejo8591@gmail.com>
2012-08-26 02:34:41 -0500

    url routes

--- node/ser.js
+++ node/ser.js
@@ -1,5 +1,24 @@
 var http = require('http');
+var path = require('path');
 http.createServer(function (request, response){
-	response.writeHead(200, {'Content-Type':'text/html'});
-	response.end('omnia Sunt Communia');
+	var pages = [
+		{route: '', output: 'omnia sunt communia'},
+		{route: 'about', output: 'communia sent'},
+		{route: 'another page', output: function()
+		{
+			return 'Here\'s'+this.route;
+		}},
+	];
+	var lookup = path.basename(decodeURI(request.url));
+	pages.forEach(function(page){
+		if(page.route === lookup){
+			response.writeHead(200, {'Content-Type':'text/html'});
+			response.end(typeof page.output === 'function'
+				? page.output():page.output);
+		}
+		});
+		if(!response.finished){
+				response.writeHead(404);
+				response.end('Page Not Found!');
+			}
 }).listen(3333);
