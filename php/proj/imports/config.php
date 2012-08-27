585ad3dde95fecf2e02d0a6ef4ab408983c6aeaf
Alejandro Romero <alejo8591@gmail.com>
2012-08-27 17:01:05 -0500

    setup for app

---
+++ php/proj/imports/config.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+/*=========== Database Configuraiton ==========*/
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'root';
+$db_name = 'mobileex';
+$db_port = '8889';
+/*=========== Website Configuration ==========*/
+
+$defaultTitle = 'Yeah! App';
+$defaultFooter = date('Y').' Copyleft';
+
+?>
