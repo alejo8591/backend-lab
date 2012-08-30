15425d5d27aaca44b29ee2061a6f663f2520b523
Alejandro Romero <alejo8591@gmail.com>
2012-08-30 14:31:59 -0500

    update file for aes server

--- php/mob2/casas.php
+++ php/mob2/casas.php
@@ -1,7 +1,7 @@
 <?php
 include_once "aes.class.php";
 include_once "aesctr.class.php";
-$timer = microtime(true);
+// $timer = microtime(true);
 $pw = '54321';
 (int) $decr = AesCtr::decrypt($_GET['id'], $pw, 256);
 
@@ -9,7 +9,7 @@ process($decr);
 
 function process($id){
     
-    $db = mysql_connect('127.0.0.1', '', '');
+    $db = mysql_connect('127.0.0.1', 'root', '3193115');
     if(!$id){
         die('Could no connect: ' . mysql_error());
     }
@@ -22,12 +22,24 @@ function process($id){
         if(!$result){
             die('Could not query:' . mysql_error());
         }
-        
+/*
     $row = mysql_fetch_object($result);
     $jsondata['id'] = $row->id;
     $jsondata['m2'] = $row->m2;
     $jsondata['hab'] = $row->hab;
     $jsondata['direccion'] = $row->direccion;
+    
+    mysql_close($db);
+    
+    echo json_encode($jsondata);
+}
+*/
+    $pw = '54321';
+    $row = mysql_fetch_object($result);
+    $jsondata['id'] = AesCtr::encrypt($row->id, $pw, 256);
+    $jsondata['m2'] = AesCtr::encrypt($row->m2, $pw, 256);
+    $jsondata['hab'] = AesCtr::encrypt($row->hab, $pw, 256);
+    $jsondata['direccion'] = AesCtr::encrypt($row->direccion, $pw, 256);
     
     mysql_close($db);
     
