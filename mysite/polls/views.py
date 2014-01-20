827e866d3d980147f39522c60ad89f61e01cc9ff
Alejandro Romero <alejo8591@gmail.com>
2014-01-20 16:17:01 -0500

    Update .gitignore file

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,11 +4,8 @@ from polls.models import Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('index.html')
-	context = RequestContext(request, {
-			'latest_poll_list' : latest_poll_list,
-		})
-	return HttpResponse(template.render(context))
+	output = ', '.join([p.question for p in latest_poll_list])
+	return HttpResponse(output)
 
 def detail(request, poll_id):
 	return HttpResponse("Your looking details of polls %s" % poll_id)
