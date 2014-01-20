7bba505d2b6c84da065724d4e848ec20656a7184
Alejandro Romero <alejo8591@gmail.com>
2014-01-20 18:01:23 -0500

    Update views in polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,22 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext, loader
 from polls.models import Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	output = ', '.join([p.question for p in latest_poll_list])
-	return HttpResponse(output)
+	context = RequestContext(request, {
+			'latest_poll_list' : latest_poll_list,
+		})
+	return render(request, 'index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse("Your looking details of polls %s" % poll_id)
+	try:
+		poll = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+
+	return render(request, 'detail.html', {'poll':poll})
 
 def results(request, poll_id):
 	return HttpResponse("Your looking result of polls %s" % poll_id)
