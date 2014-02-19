6e454814d54bb7f652284734c8379e69cb565b94
Alejandro Romero <alejandro.romero@agilapps.co>
2014-02-19 15:03:57 -0500

    Update test case for contacts views

--- tut/contacts/tests.py
+++ tut/contacts/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.test.client import Client, RequestFactory
 from contacts.models import Contact
+from contacts.views import *
 
 class ContactTest(TestCase):
 	""" Contact models tests. """
@@ -9,3 +11,28 @@ class ContactTest(TestCase):
 		contact = Contact(first_name='Alejandro', last_name='Romero')
 
 		self.assertEquals(str(contact), 'Alejandro Romero', )
+
+class ContactListViewTest(TestCase):
+	""" Contact list view test """
+	def test_contacts_in_the_context(self):
+		client = Client()
+		response = client.get('/')
+
+		self.assertEquals(list(response.context['object_list']), [])
+
+		Contact.objects.create(first_name='Camilo', last_name='forro')
+
+		response = client.get('/')
+		self.assertEquals(response.context['object_list'].count(), 1)
+
+	def test_contact_in_the_context_request_factory(self):
+		factory = RequestFactory()
+		request = factory.get('/')
+
+		response = ListContactView.as_view()(request)
+
+		self.assertEquals(list(response.context_data['object_list']), [])
+
+		Contact.objects.create(first_name='Pam', last_name='Per')
+		response = ListContactView.as_view()(request)
+		self.assertEquals(response.context_data['object_list'].count(), 1)
