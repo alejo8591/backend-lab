a160e13daa2c44c936f4e46cae8495d21adeef2c
Alejandro Romero <alejo8591@gmail.com>
2014-03-20 15:44:20 -0500

    Include categories templates and views

--- its/bevolist/urls.py
+++ its/bevolist/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 	url(r'^$', 'bevolist.views.index', name='bevolist-index'),
 	url(r'^(?P<item_id>\d+)/$', 'bevolist.views.details', name='bevolist-details'),
+	url(r'^category/$', 'bevolist.views.category', name='bevolist-category'),
+	url(r'^category/(?P<slug>[\w]+)/$', 'bevolist.views.category_details', name='bevolist-category-details'),
 )
--- its/bevolist/views.py
+++ its/bevolist/views.py
@@ -15,3 +15,19 @@ def details(request, item_id):
 		'item' : item
 	})
 	return render_to_response('bevolist/details.html', context)
+
+def category(request):
+	categories = Category.objects.all()
+	context = Context({'title' : 'Categories List', 'categories' :categories})
+
+	return render_to_response('bevolist/categories.html', context)
+
+def category_details(request, slug):
+	category = get_object_or_404(Category, slug=slug)
+
+	context = Context({
+		'title' : 'Category: %s' %category.name,
+		'category' : category
+	})
+
+	return render_to_response('bevolist/category.html', context)
