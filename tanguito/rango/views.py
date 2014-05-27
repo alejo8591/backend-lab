9143408cf7f910ad4b9308333fb4328351d614b8
Alejandro Romero <alejo8591@gmail.com>
2014-05-27 14:47:23 -0500

    Update application with templates

--- tanguito/rango/views.py
+++ tanguito/rango/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+
+	context = RequestContext(request)
+
+	context_dict = {'boldmessage' : "I am a bold context"}
+
+	return render_to_response('index.html', context_dict, context)
