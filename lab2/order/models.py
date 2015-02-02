5d807d2dbe07928626c5eebd2cfe0cebb1472c03
Alejandro Romero <alejo8591@gmail.com>
2015-02-02 18:40:46 -0500

    Update Models for order, and migrate

--- lab2/order/models.py
+++ lab2/order/models.py
@@ -4,15 +4,15 @@ from django.db import models
 class Customer(models.Model):
     customer_name = models.CharField(max_length=128,
                                     blank=True, null=True,
-                                    verbose_name='Name',
+                                    verbose_name='Nombre',
                                     help_text='Ingrese el Nombre Completo.')
 
-    customer_address = models.CharFiled(max_length=64,
+    customer_address = models.CharField(max_length=64,
                                     blank=True, null=True,
                                     verbose_name='Dirección',
                                     help_text='Ingrese la Direccion Completa.')
 
-    customer_phone = models.CharFiled(max_length=24,
+    customer_phone = models.CharField(max_length=24,
                                     blank=True, null=True,
                                     verbose_name='Teléfono',
                                     help_text='Ingrese el Teléfono.')
@@ -21,7 +21,29 @@ class Customer(models.Model):
         return self.customer_name
 
 
-class Stock(models.Product):
+class Product(models.Model):
+    product_name = models.CharField(max_length=128,
+                                    verbose_name='Nombre del Producto',
+                                    help_text='Indique Nombre del Producto.')
+
+    product_price = models.DecimalField(max_digits=64,
+                                    decimal_places = 2,
+                                    verbose_name='Precio',
+                                    help_text='Precio del Producto.')
+
+    product_type = models.CharField(max_length=128,
+                                    verbose_name='Tipo de Producto',
+                                    help_text='Indique el Tipo de Producto.')
+
+    product_description = models.TextField(max_length=128,
+                                    verbose_name='Descripción de Producto',
+                                    help_text='Indique el Tipo de Producto.')
+
+    def __str__(self):
+        return self.product_name
+
+
+class Stock(models.Model):
     stock_product_id = models.ForeignKey('Product',
                                     verbose_name='producto',
                                     help_text='Stock del Producto.')
@@ -31,34 +53,13 @@ class Stock(models.Product):
                                     help_text='Cantidad del Producto.')
 
 
-class Product(models.Model):
-    product_price = models.DecimalField(max_digits=64,
-                                    decimal_places = 2,
-                                    verbose_name='Precio',
-                                    help_text='Precio del Producto.')
-
-    product_name = models.CharFiled(max_length=128,
-                                    verbose_name='Nombre del Producto',
-                                    help_text='Indique Nombre del Producto.')
-
-    product_type = models.CharFiled(max_length=128,
-                                    verbose_name='Tipo de Producto',
-                                    help_text='Indique el Tipo de Producto.')
-
-    product_description = models.TextField(max_length=128,
-                                    verbose_name='Descripción de Producto',
-                                    help_text='Indique el Tipo de Producto.')
-
-    def __str__(self):
-        return self.customer_name
-
 class Order(models.Model):
     order_customer_id = models.ForeignKey('Customer',
                                     verbose_name='Cliente',
                                     help_text='Seleccione el Nombre del Cliente.')
 
     order_product_id = models.ForeignKey('Product',
-                                    verbose_name='Prodcuto',
+                                    verbose_name='Producto',
                                     help_text='Seleccione el Producto(s).')
 
     order_amount = models.DecimalField(max_digits=64,
@@ -69,4 +70,4 @@ class Order(models.Model):
     order_date = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.id
+        return str(self.order_customer_id)
